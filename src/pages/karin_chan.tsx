aa1ec461f11d74393743c7aa14a848c4f0c239c9
Winson Cheng <83229800+0xKMG@users.noreply.github.com>
2023-08-20 10:22:40 +0800

    feat: add test page for profile

---
+++ src/pages/karin_chan.tsx
@@ -0,0 +1,49 @@
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
+
+export default About;
+
+// ['karin_chan',
+//  'eric_san',
+//  'michael_yau',
+//  'winson_cheng',
+//  'wesley_chan',
+//  'karen_tsoi',
+//  'katherina_nguyen',
+//  'ricky_chan',
+//  'wright_chin',
+//  'corliss_lam',
+//  'kk_chan',
+//  'anna_khomenko',
+//  'kit_tang',
+//  'tom_mong',
+//  'bryan_lam',
+//  'ben_ng',
+//  'thomas',
+//  'suresh_thapa',
+//  'christine_he',
+//  'lentz_chun',
+//  'edmond_fung',
+//  'stephen_yin_sik_go',
+//  'wilson_yeh',
+//  'ka_pui_ng',
+//  'lok_yeung',
+//  'madina_jamolova',
+//  'lawrence_wong',
+//  'lester_mok',
+//  'jason',
+//  'keith_engine',
+//  'bennett_chan',
+//  'franka_robin_z',
+//  'michael_chik',
+//  'muhammad_saad_shahid_anwel',
+//  'felix_wong',
+//  'francis_tse',
+//  'ying_hong',
+//  'felix_yu']
