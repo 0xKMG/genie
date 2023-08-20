5c1a8214d12315ca0906801647fc12d798996e9a
Winson Cheng <83229800+0xKMG@users.noreply.github.com>
2023-08-20 17:08:18 +0800

    feat: add button for genie wish

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -5,7 +5,18 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <div className="container mx-auto px-4">{children}</div>
+  <div className="container mx-auto px-4">
+    {children}
+
+    <div className="mt-4 flex justify-center">
+      <a
+        href="swhk.geniefriends.io"
+        className="rounded bg-blue-500 px-6 py-2 font-bold text-white hover:bg-blue-700"
+      >
+        Make Your Wish
+      </a>
+    </div>
+  </div>
 );
 
 export default Layout;
--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -22,34 +22,27 @@ interface PostProps {
 export default function Post({ postData }: PostProps) {
   return (
     <Layout>
-      <div className="flex flex-col items-center space-y-4 p-4 md:space-y-8">
-        {/* Profile Picture Placeholder */}
-        <div className="h-32 w-32 rounded-full bg-gray-300 md:h-48 md:w-48"></div>
-
+      <div className="flex flex-col items-center space-y-4 p-4 font-serif md:space-y-8 ">
+        <div>
+          <img
+            src="/assets/logo.png"
+            alt="Company Logo"
+            className="center h-auto w-20" // or any other size you prefer
+          />
+        </div>
+        {/* Profile Picture */}
+        <div className="relative h-32 w-32 overflow-hidden rounded-full md:h-48 md:w-48">
+          <img
+            src={`/assets/${postData.slug}.jpg`} // Change this based on your setup
+            alt={postData.name}
+            className="absolute left-0 top-0 h-full w-full object-cover"
+          />
+        </div>
         <h1 className="text-2xl font-semibold md:text-4xl">{postData.name}</h1>
         <p className="md:text-md text-sm text-gray-600">
           {postData.two_word_summary}
         </p>
 
-        <div className="w-full space-y-2 md:space-y-4">
-          <p>
-            <span className="font-semibold">Team:</span> {postData.team}
-          </p>
-          <p>
-            <span className="font-semibold">Attended:</span>{' '}
-            {postData.attended_event}
-          </p>
-          <p>
-            <span className="font-semibold">Summary:</span>{' '}
-            {postData.linkedin_summary_gpt4}
-          </p>
-          <p>
-            <span className="font-semibold">Career Highlight:</span>{' '}
-            {postData.linkedin_career_highlight_gpt4}
-          </p>
-        </div>
-
-        {/* LinkedIn Profile Link with Icon */}
         <a
           href={postData.linkedin}
           className="flex items-center space-x-2 text-gray-700 hover:text-gray-700"
@@ -65,6 +58,12 @@ export default function Post({ postData }: PostProps) {
             <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
           </svg>
         </a>
+        <div className="w-full space-y-2 md:space-y-4">
+          {/* <p>
+            <span className="font-semibold">Team:</span> {postData.team}
+          </p> */}
+          <p>{postData.linkedin_summary_gpt4}</p>
+        </div>
       </div>
     </Layout>
   );
