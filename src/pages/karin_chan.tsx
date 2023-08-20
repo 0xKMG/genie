8d92c9fc911254b141ac6c17ab3e42a3c5832c4a
Winson Cheng <83229800+0xKMG@users.noreply.github.com>
2023-08-20 12:23:23 +0800

    test code for api call

--- src/pages/karin_chan.tsx
+++ src/pages/karin_chan.tsx
@@ -1,49 +1,43 @@
+import React, { useEffect, useState } from 'react';
+
+type APIError = {
+  message: string;
+};
+
 function About() {
+  const [apiData, setApiData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [apiError, setApiError] = useState<APIError | null>(null);
+
+  useEffect(() => {
+    fetch(
+      'http://112.119.146.38:8000/ask_genie_suhk?query=I%20wish%20to%20find%20a%20ux%20designer&model=gpt-4',
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((responseData) => {
+        setApiData(responseData);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setApiError({ message: err.message });
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (apiError) return <div>Error: {apiError.message}</div>;
+
   return (
     <div>
-      <h1>About Page</h1>
-      <p>This is the about page.</p>
+      <h2>Data from API</h2>
+      <pre>{JSON.stringify(apiData, null, 2)}</pre>
     </div>
   );
 }
 
 export default About;
-
-// ['karin_chan',
-//  'eric_san',
-//  'michael_yau',
-//  'winson_cheng',
-//  'wesley_chan',
-//  'karen_tsoi',
-//  'katherina_nguyen',
-//  'ricky_chan',
-//  'wright_chin',
-//  'corliss_lam',
-//  'kk_chan',
-//  'anna_khomenko',
-//  'kit_tang',
-//  'tom_mong',
-//  'bryan_lam',
-//  'ben_ng',
-//  'thomas',
-//  'suresh_thapa',
-//  'christine_he',
-//  'lentz_chun',
-//  'edmond_fung',
-//  'stephen_yin_sik_go',
-//  'wilson_yeh',
-//  'ka_pui_ng',
-//  'lok_yeung',
-//  'madina_jamolova',
-//  'lawrence_wong',
-//  'lester_mok',
-//  'jason',
-//  'keith_engine',
-//  'bennett_chan',
-//  'franka_robin_z',
-//  'michael_chik',
-//  'muhammad_saad_shahid_anwel',
-//  'felix_wong',
-//  'francis_tse',
-//  'ying_hong',
-//  'felix_yu']
