9a238053c291f6a08c4b0cb7cd5b347502c2c3e5
Winson Cheng <83229800+0xKMG@users.noreply.github.com>
2023-08-20 15:27:04 +0800

    add basic styling for the profile

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -22,16 +22,54 @@ interface PostProps {
 export default function Post({ postData }: PostProps) {
   return (
     <Layout>
-      <h1>{postData.name}</h1>
-      <p>Team: {postData.team}</p>
-      <p>Attended: {postData.attended_event}</p>
-      <p>Summary: {postData.linkedin_summary_gpt4}</p>
-      <p>Career Highlight: {postData.linkedin_career_highlight_gpt4}</p>
-      <p>Description: {postData.two_word_summary}</p>
-      <a href={postData.linkedin}>LinkedIn Profile</a>
+      <div className="flex flex-col items-center space-y-4 p-4 md:space-y-8">
+        {/* Profile Picture Placeholder */}
+        <div className="h-32 w-32 rounded-full bg-gray-300 md:h-48 md:w-48"></div>
+
+        <h1 className="text-2xl font-semibold md:text-4xl">{postData.name}</h1>
+        <p className="md:text-md text-sm text-gray-600">
+          {postData.two_word_summary}
+        </p>
+
+        <div className="w-full space-y-2 md:space-y-4">
+          <p>
+            <span className="font-semibold">Team:</span> {postData.team}
+          </p>
+          <p>
+            <span className="font-semibold">Attended:</span>{' '}
+            {postData.attended_event}
+          </p>
+          <p>
+            <span className="font-semibold">Summary:</span>{' '}
+            {postData.linkedin_summary_gpt4}
+          </p>
+          <p>
+            <span className="font-semibold">Career Highlight:</span>{' '}
+            {postData.linkedin_career_highlight_gpt4}
+          </p>
+        </div>
+
+        {/* LinkedIn Profile Link with Icon */}
+        <a
+          href={postData.linkedin}
+          className="flex items-center space-x-2 text-gray-700 hover:text-gray-700"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-linkedin"
+            viewBox="0 0 16 16"
+          >
+            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+          </svg>
+        </a>
+      </div>
     </Layout>
   );
 }
+
 function getAllPostIds() {
   return attendees.map((attendee) => {
     return {
