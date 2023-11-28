b2e1c58b69d5030d6a77f378d372ca331abc617b
Winson Cheng <83229800+0xKMG@users.noreply.github.com>
2023-11-28 20:32:02 +0800

    testing

--- src/pages/apiTest.tsx
+++ src/pages/apiTest.tsx
@@ -1,8 +1,15 @@
 // pages/api-demo.js
 import React, { useEffect, useState } from 'react';
 
+interface ApiResponse {
+  name: string;
+  key: string;
+  profile_pic: string;
+  linkedin_url: string;
+}
+
 const APIDemo = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<ApiResponse[]>([]);
   const [loading, setLoading] = useState(true);
   const [fetchError, setFetchError] = useState('');
 
@@ -10,12 +17,13 @@ const APIDemo = () => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          'https://b726-218-189-75-29.ngrok-free.app/dummy_members',
+          'https://ml.aska.ai/geniefriends/dummy_members?passcode=magic',
         );
         if (!response.ok) {
           throw new Error(`Failed to fetch, status: ${response.status}`);
         }
         const jsonData = await response.json();
+
         setData(jsonData);
         setLoading(false);
       } catch (error) {
@@ -45,7 +53,29 @@ const APIDemo = () => {
   return (
     <div>
       <h1>API Data</h1>
-      <pre>{data}</pre>
+      {data && Array.isArray(data) ? (
+        <ul>
+          {data.map((item, index) => (
+            <li key={index}>
+              <p>Name: {item.name}</p>
+              <p>Key: {item.key}</p>
+              <img src={item.profile_pic} alt={`Profile of ${item.name}`} />
+              <p>
+                LinkedIn:
+                <a
+                  href={item.linkedin_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {item.linkedin_url}
+                </a>
+              </p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Data is not in the expected format.</p>
+      )}
     </div>
   );
 };
